2309c5ac8d7d55d8df09fd7d0a200cc8fa385b35
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-12 16:44:01 -0300

    Adiciona pequenas modificações nos nomes das variáveis

--- DesafiosSysManager/src/desafios/DesafioDois.java
+++ DesafiosSysManager/src/desafios/DesafioDois.java
@@ -6,23 +6,23 @@ public class DesafioDois {
 	public static void main(String[] args) {
 	
 	float[] numfloat8 = {8.7f, 7.8f};
-	byte[] numeros = {3, 5, 7};
+	byte[] numbyte = {3, 5, 7};
 	
-	float mediaFloat = 0f;
+	float mediaFloat = 0;
 	byte mediaByte = 0;
-	float somaMedia = 0f;
-	float mediaTotal = 0f;
+	float somaMedia = 0;
+	float mediaTotal = 0;
 	
 	mediaFloat = (numfloat8[0] + numfloat8[1])/2;
-	mediaByte = (byte) ((numeros[0] + numeros[1] + numeros[2])/3);
+	mediaByte = (byte) ((numbyte[0] + numbyte[1] + numbyte[2])/3);
 	somaMedia = mediaFloat + mediaByte;
 	mediaTotal = (somaMedia/2);
 	
 	System.out.println("Média dos números " + numfloat8[0] + " e " + numfloat8[1] 
 			+ " é igual a: " + mediaFloat);
 	
-	System.out.println("Média dos números " + numeros[0] + " e " + numeros[1] 
-			+ numeros[2] + " é igual a: " + mediaByte);
+	System.out.println("Média dos números " + numbyte[0] + " e " + numbyte[1] 
+			+ numbyte[2] + " é igual a: " + mediaByte);
 	
 	System.out.println("Soma das médias " + mediaFloat + " e " + mediaByte 
 			+ " é igual a: " + somaMedia);
