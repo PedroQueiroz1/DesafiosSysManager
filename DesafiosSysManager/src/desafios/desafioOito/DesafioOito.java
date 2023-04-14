1da5173d2b1c7f2874782139f787399af6e2bc36
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-13 21:12:42 -0300

    Adiciona melhorias para a resolucao do oitavo desafio

--- DesafiosSysManager/src/desafios/desafioOito/DesafioOito.java
+++ DesafiosSysManager/src/desafios/desafioOito/DesafioOito.java
@@ -1,6 +1,9 @@
 package desafios.desafioOito;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 import desafios.desafioOito.entidade.Candidato;
@@ -21,9 +24,10 @@ public class DesafioOito {
 		
         byte num = 0;
 		
+
 		do {
-				System.out.println("Escolha o número do candidato a ser votado. (De 1 a 5)");
-				num = scanner.nextByte();	
+			System.out.println("Escolha o número do candidato a ser votado. (De 1 a 5)");
+			num = scanner.nextByte();	
 			
 			if(num>=1 && num<=5) {
 			candidato[num].setVoto((byte) (candidato[num].getVoto() + 1));
@@ -33,12 +37,38 @@ public class DesafioOito {
 			
 		}while(num != 0);
 		
+	
+		/*
+		 * forEach
+		 * Imprime a lista de candidatos
+		 */
 		for(Candidato c : candidato) {
 			if(c != null) {
 				System.out.println(c.toString());
 			}
 		}
 		
+		
+		//------- Aprendendo um pouco mais de Stream API ---------
+		List<Candidato> candidatoList = Arrays.asList(candidato);
+		
+		/*
+		 * O filter está filtrando os elementos que não são nulos
+		 * o max obtém o candidato com o maior número de votos
+		*/
+		Candidato ganhador = candidatoList.stream()
+				.filter(c -> c != null)
+				.max(Comparator.comparing(Candidato::getVoto))
+				.orElse(null);
+		
+		if(ganhador != null) {
+			System.out.println("O ganhador é o " + ganhador.getNome() + "!!!");
+		}else {
+			System.out.println("Não teve ganhador...");
+		}
+		
+		
+
 		scanner.close();
 		
 	}
